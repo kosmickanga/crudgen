d888418348f943a206e916398d57838251f28489
Bruce Horn <kangabroo@gmail.com>
2020-09-06 10:20:18 +0100

    Add project files.

---
+++ CrudGen/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrudGen
+{
+    public class Class
+    {
+        public string Name { get; set; }
+        public List<Field> Fields { get; set; }
+    }
+
+    public class Field
+    {
+        public string Name { get; set; }
+        public bool Required { get; set; }
+        public string DataType { get; set; }
+
+        public string DefaultValue { get; set; }
+    }
+}
